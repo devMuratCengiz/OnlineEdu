f09ace4281c84af2d1fd7537572436fbc73352c2
Murat Cengiz <devmuratcengiz@outlook.com>
2025-08-18 15:21:47 +0300

    Add Entity Framework Core and new data models
    Updated project to include Entity Framework Core with necessary package references. Created `OnlineEduContext` for managing database entities and added migration class `mig1` for table creation. Introduced `ValuesController` for retrieving connection strings and updated `appsettings.json` with SQL Server connection details. Added new entity classes for `About`, `Banner`, `Blog`, `BlogCategory`, `Contact`, `Course`, `CourseCategory`, `Message`, `SocialMedia`, `Subscriber`, and `Testimonial`. Modified project structure to enhance inter-component references.

---
+++ OnlineEdu.Entity/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Entity.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public bool IsShown { get; set; }
+
+        public int CourseCategoryId { get; set; }
+        public CourseCategory CourseCategory { get; set; }
+    }
+}
