2942bd16105845c88400ad61d052867f012b15d3
Murat Cengiz <devmuratcengiz@outlook.com>
2025-09-10 13:55:04 +0300

    Contact page edited, separated into map view components.

---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactInfoMap.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.ContactDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactInfoMap : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var value = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(value);
+        }
+    }
+}
